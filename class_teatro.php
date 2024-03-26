1a171c356c1f042fff2fda21f9111fdc3fabb534
Valentin Bustos Villar <valentinbv29@gmail.com>
2024-03-26 11:50:10 -0300

    terminame de formatear el texto y esta listo para empezar el tp2

--- class_teatro.php
+++ class_teatro.php
@@ -1,48 +1,55 @@
-<?php 
-class teatro{
+<?php
+class teatro
+{
     private string $nombreTeatroInt;
     private string $direccionTeatroInt;
     private array $funcionesTeatroInt;
 
-    public function __construct($nombreTeatroExt,$direccionTeatroExt,$funcionesTeatroExt){
+    public function __construct($nombreTeatroExt, $direccionTeatroExt, $funcionesTeatroExt)
+    {
         $this->nombreTeatroInt = $nombreTeatroExt;
         $this->direccionTeatroInt = $direccionTeatroExt;
         $this->funcionesTeatroInt = $funcionesTeatroExt;
     }
 
-    public function getNombreTeatro(){
+    public function getNombreTeatro()
+    {
         return $this->nombreTeatroInt;
     }
 
-    public function getDireccionTeatro(){
+    public function getDireccionTeatro()
+    {
         return $this->direccionTeatroInt;
     }
 
-    public function getFuncionEspecificaTeatro($posicion){
+    public function getFuncionEspecificaTeatro($posicion)
+    {
         return $this->funcionesTeatroInt[$posicion];
     }
 
-    public function setFuncionEspecificaTeatro($posicion, $nuevoTitulo, $nuevoPrecio){
+    public function setFuncionEspecificaTeatro($posicion, $nuevoTitulo, $nuevoPrecio)
+    {
         $this->funcionesTeatroInt[$posicion]["nombre"] = $nuevoTitulo;
         $this->funcionesTeatroInt[$posicion]["precio"] = $nuevoPrecio;
     }
 
-    public function setNombreTeatro($nuevoNombre){
+    public function setNombreTeatro($nuevoNombre)
+    {
         $this->nombreTeatroInt = $nuevoNombre;
     }
-    
-    public function setDireccionTeatro($nuevaDireccion){
+
+    public function setDireccionTeatro($nuevaDireccion)
+    {
         $this->direccionTeatroInt = $nuevaDireccion;
     }
 
 
     public function __toString()
     {
-            return "";
+        return "";
     }
-
 }
-$arrayFunciones = [["nombre"=> "ranas magicas", "precio" => 40],["nombre"=> "palos malvados", "precio" => 10],["nombre"=> "revolucion hormiga", "precio" => 10],["nombre"=> "carceles embotelladas","precio" => 10]];
+$arrayFunciones = [["nombre" => "ranas magicas", "precio" => 40], ["nombre" => "palos malvados", "precio" => 10], ["nombre" => "revolucion hormiga", "precio" => 10], ["nombre" => "carceles embotelladas", "precio" => 10]];
 
 $obj = new teatro("magistral", "Ave. Buenos Aires", $arrayFunciones);
 echo $obj;
